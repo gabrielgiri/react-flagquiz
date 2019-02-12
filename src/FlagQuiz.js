6c8531a1e464800fb03950951777be51becad783
Gabriel Giri <gabrielgiri@gmail.com>
2019-02-12 10:54:19 -0300

    bootstrapping the flag quiz

---
+++ src/FlagQuiz.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import './bootstrap.min.css';
+
+class FlagQuiz extends Component {
+  render() {
+    return (
+      <div>Flag Quiz</div>
+    );
+  }
+}
+
+export default FlagQuiz;
